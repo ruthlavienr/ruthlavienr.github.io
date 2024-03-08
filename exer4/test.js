afb98c48799c88eef8f6ce2262456ad500833c52
ruthlavienr <retenifrancia@up.edu.ph>
2024-03-08 12:48:56 +0800

    chore: added new test cases

--- exer4/test.js
+++ exer4/test.js
@@ -6,3 +6,5 @@ import index from './index.js';
 console.log(index.addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));
 console.log(index.addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]));
 console.log(index.addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]));
+console.log(index.addAccount(["Tim", "Berners-Lee", "tic.com", 25]));
+console.log(index.addAccount(["Ted", "Nelson", "ted.n@w3c.com", 4]));

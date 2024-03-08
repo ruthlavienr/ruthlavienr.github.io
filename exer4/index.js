9c624afc692d7c861489d79955a37d6bc5892e70
ruthlavienr <retenifrancia@up.edu.ph>
2024-03-08 12:48:10 +0800

    docs: added documentation

--- exer4/index.js
+++ exer4/index.js
@@ -1,9 +1,13 @@
+//Ruth Djusthine E. Tenifrancia
+//UV-3L
+
+//Imports
 import { v4 as uuidv4 } from 'uuid';
 import validator from 'validator';
 import isEmail from 'validator/lib/isEmail.js';
 import { appendFileSync } from 'node:fs';
 
-function generateUniqueID(fname,lname){
+function generateUniqueID(fname,lname){ //function the gets the first letter of first name, lowercase of last name, and 8 characters of randomized uuid
     const str = 'bobbyhadz.com';
     const letter = str.charAt(0);
     const lowLname = lname.toLowerCase();
@@ -13,7 +17,7 @@ function generateUniqueID(fname,lname){
 }
 
 
-function addAccount(arr){
+function addAccount(arr){ //this function add account to the users.txt
     var fname= false;
     var lname= false;
     var email= false;
@@ -21,25 +25,15 @@ function addAccount(arr){
     var j=-1;
     var k=-2;
     
-    console.log(arr.length)
-    console.log(arr[0])
-    console.log(arr[1])
-    console.log(arr[2])
-    if(arr.length==4 && arr[0] !== '' && arr[1] !== '' && arr[2] !== '' && arr[3]>17){
-        
-        console.log(true)
-        if(validator.isEmail(arr[2])){
-            console.log(true)
-            var data = arr[0] + ',' + arr[1] + ',' + arr[2] + ',' + arr[3] + ',' + generateUniqueID(arr[0],arr[1])        
-            appendFileSync("users.txt", data + '\n', {'flags': 'a+'});
+    if(arr.length==4 && arr[0] !== '' && arr[1] !== '' && arr[2] !== '' && arr[3]>17){ //validation
+        if(validator.isEmail(arr[2])){ //checks if email
+            var data = arr[0] + ',' + arr[1] + ',' + arr[2] + ',' + arr[3] + ',' + generateUniqueID(arr[0],arr[1])       //data variable 
+            appendFileSync("users.txt", data + '\n', {'flags': 'a+'}); //appends to file
             return(true);
         }else{
-            console.log(false)
             return(false);
         }
     }else{
-        
-        console.log(false)
         return(false);
     }
     
--- exer4/test.js
+++ exer4/test.js
@@ -1,5 +1,8 @@
+//Ruth Djusthine E. Tenifrancia
+//UV-3L
 import index from './index.js';
 
+//test cases
 console.log(index.addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));
 console.log(index.addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]));
 console.log(index.addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]));

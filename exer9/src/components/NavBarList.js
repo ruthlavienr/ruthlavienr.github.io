1e22c278067936e75ec0e218684ed1261b169678
ruthlavienr <retenifrancia@up.edu.ph>
2024-04-30 20:04:13 +0800

    style: converted class into a function

--- exer9/src/components/NavBarList.js
+++ exer9/src/components/NavBarList.js
@@ -8,17 +8,14 @@ jsx file for navigation bar component
 import React from "react";
 import NavBtn from "./NavBtn";
 
-class NavBarList extends React.Component{
-
-    render() {
-        return(
-            //html/jsx for navigation bar
-            <ul>
-                <li className="webTitle">{this.props.title}</li>
-                <NavBtn attri={this.props.attri}/>
-            </ul>          
-        );
-    }
+function NavBarList(props) {
+    return (
+        //html/jsx for navigation bar
+        <ul>
+            <li className="webTitle">{props.title}</li>
+            <NavBtn attri={props.attri}/>
+        </ul>
+    );
 }
 
 export default NavBarList;
--- exer9/src/components/NavBtn.js
+++ exer9/src/components/NavBtn.js
@@ -7,20 +7,18 @@ jsx file for navigation button
 
 import React from "react";
 
-class NavBtn extends React.Component {
-    render() {
-        // Variable for cards list
-        const lists = this.props.attri;
+function NavBtn(props) {
+    // Variable for cards list
+    const lists = props.attri;
 
-        return (
-            <>
-                {lists.map((list, index) => (
-                    // For each clickable element of the navbar menu
-                    <li key={index}><a href={list.url}>{list.name}</a></li>
-                ))}
-            </>
-        );
-    }
+    return (
+        <>
+            {lists.map((list, index) => (
+                // For each clickable element of the navbar menu
+                <li key={index}><a href={list.url}>{list.name}</a></li>
+            ))}
+        </>
+    );
 }
 
 export default NavBtn;
